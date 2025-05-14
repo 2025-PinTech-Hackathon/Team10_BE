110b56a53f12faa896f618714dc41107235579a8
woongjee <jeewoong7753@naver.com>
2025-05-14 19:39:49 +0900

    feat: 초기 세팅

---
+++ enp/src/main/java/org/example/enp/EnpApplication.java
@@ -0,0 +1,13 @@
+package org.example.enp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnpApplication.class, args);
+    }
+
+}

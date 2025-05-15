5efbf493abaa439662d613682baad497f6394727
moonino <dlsgh4090@gmail.com>
2025-05-15 13:19:41 +0900

    feat: 초기 세팅

--- enp/src/main/java/com/enp/domain/dto/response/SignupResponseDTO.java
+++ enp/src/main/java/com/enp/domain/dto/response/SignupResponseDTO.java
@@ -1,2 +1,17 @@
-package com.enp.domain.dto.response;public class SignupResponseDTO {
+package com.enp.domain.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupResponseDTO {
+    private Long userId;
+    private Boolean isDuplicated;
+
 }
--- enp/src/main/java/com/enp/service/UserService.java
+++ enp/src/main/java/com/enp/service/UserService.java
@@ -1,8 +1,31 @@
 package com.enp.service;
 
+import com.enp.domain.dto.request.SignupRequestDto;
+import com.enp.domain.dto.response.SignupResponseDTO;
+import com.enp.domain.entity.User;
+import com.enp.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
+    public UserRepository userRepository;
+    public SignupResponseDTO signupService(SignupRequestDto signupRequestDto){
+        boolean isExist=userRepository.existsByLoginId(signupRequestDto.getLoginId());
+        if(!isExist){
+            User user=User.builder()
+                    .nickname(signupRequestDto.getNickname())
+                    .loginId(signupRequestDto.getLoginId())
+                    .password(signupRequestDto.getPassword())
+                    .build();
+            userRepository.save(user);
+            return SignupResponseDTO.builder()
+                    .userId(user.getId())
+                    .isDuplicated(false)
+                    .build();
+        }
+        return  SignupResponseDTO.builder()
+                .userId(null)
+                .isDuplicated(true)
+                .build();
+    }
 }

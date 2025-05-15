8852c195a4fa8ee7e71d6a922395ce8efac823e7
nayoung <dlskdud0311@naver.com>
2025-05-15 20:20:35 +0900

    fix: NewspaperController 경로 수정함

--- enp/src/main/java/com/enp/controller/NewspaperController.java
+++ enp/src/main/java/com/enp/controller/NewspaperController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/news/{userId}")
+@RequestMapping("/news")
 public class NewspaperController {
     private final NewspaperService newspaperService;
 

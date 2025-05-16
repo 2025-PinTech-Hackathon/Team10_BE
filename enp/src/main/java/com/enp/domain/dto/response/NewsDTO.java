854b44278120e4f4400f9fff223eabce8af69a5f
moonino <dlsgh4090@gmail.com>
2025-05-16 18:41:58 +0900

    fix: Data 어노테이션 추가

--- enp/src/main/java/com/enp/domain/dto/response/NewsDTO.java
+++ enp/src/main/java/com/enp/domain/dto/response/NewsDTO.java
@@ -1,8 +1,10 @@
 package com.enp.domain.dto.response;
 
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
 public class NewsDTO{
     private String title;
     private String summary;

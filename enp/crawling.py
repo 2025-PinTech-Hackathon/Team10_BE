af8f4715b29a78bc2c26acc521a812f889c3241e
moonino <dlsgh4090@gmail.com>
2025-05-17 13:42:01 +0900

    fix : gpt 모델 변경

--- enp/crawling.py
+++ enp/crawling.py
@@ -64,7 +64,7 @@ def get_headline_urls():
 def simplify_text(text):
     try:
         response = openai.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4-turbo",
             messages=[
                 {"role": "system", "content": "너는 경제 뉴스를 경제에 관한 지식이 없는 사람들도 이해할 수 있을 정도로 쉬운 말로 풀이해주고 높임말로 대답해줘"},
                 {"role": "user", "content": f"다음 글을 변경해줘:\n\n{text}"}

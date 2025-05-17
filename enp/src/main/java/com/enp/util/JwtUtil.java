fc635a84165debc3b16be049084723ad7ad9fcae
moonino <dlsgh4090@gmail.com>
2025-05-17 11:17:42 +0900

    fix : jwt 수정

--- enp/src/main/java/com/enp/util/JwtUtil.java
+++ enp/src/main/java/com/enp/util/JwtUtil.java
@@ -1,11 +1,11 @@
 package com.enp.util;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.security.SignatureException;
 import java.util.Date;
 
 @Component
@@ -23,14 +23,35 @@ public class JwtUtil {
     }
 
     public String extractUsername(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+        return extractAllClaims(token).getSubject();
+    }
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+    private Boolean isTokenExpired(String token) {
+        try {
+            return extractExpiration(token).before(new Date());
+        } catch (ExpiredJwtException e) {
+            // extractExpiration 내부에서 parseClaimsJws를 호출하므로,
+            // 만료된 경우 여기서 ExpiredJwtException이 발생할 수 있습니다.
+            // 이 경우, 명백히 만료된 것이므로 true를 반환합니다.
+            return true;
+        }
     }
 
-    public boolean validateToken(String token, UserDetails userDetails) {
-        String username = extractUsername(token);
-        return username.equals(userDetails.getUsername());
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        try {
+            final String usernameFromToken = extractUsername(token); // 여기서 만료/서명 등 기본적인 검증이 일어남
+            return (usernameFromToken.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder() // 최신 API 사용
+                .setSigningKey(SECRET_KEY) // Key 객체 사용
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
